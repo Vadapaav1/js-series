fa32fd10ee87d2fbeb455e6b0197e178b574f070
Priyamvada Sharma <130129372+Vadapaav1@users.noreply.github.com>
2024-10-04 08:14:44 +0000

    container config done

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,26 @@
+let score ="samay"
+//console.log(typeof score);
+//console.log(typeof (score));
+
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+//"33" =>33
+//"33abc" => NaN
+//true =1; false =>0
+
+let isLoggedIn ="marcy"
+
+let boolenIsLoggedIn = Boolean(isLoggedIn)
+// console.log(boolenInLoggedIn);
+
+//1 =>true; 0 =>false
+//"" =>false
+//"marcy" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
